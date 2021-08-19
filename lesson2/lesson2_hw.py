71c77eb3203e7db4b4608bf07bacfaa02c4ff1fa
Dilbara Asanalieva <dilbara2025@gmail.com>
2021-08-19 18:54:07 +0600

    'hw2'

--- lesson2/lesson2_hw.py
+++ lesson2/lesson2_hw.py
@@ -1,52 +1,60 @@
 current_year = 2021
+
 class Person:
     '''Human class'''
     __total_persons = 0
-    __birth_year = 2002 #maybe input?
+    __birth_year = 2000  # maybe input?
+    __current_year = 2021
     __name = "Alice"
     __language = "spanish"
 
-
-
-    def __init__(self,name,birth_year,**kwargs):
+    def __init__(self, name, birth_year, **kwargs):
         self.name = name
         self.birth_year = birth_year
-
+        # self.__current_year = kwargs
         self.__language = kwargs
 
-        self.__total_persons +=1
-        if self.__birth_year >= current_year:
-            raise ValueError("Bigger than current year!")
-
+        #self.__total_persons += 1
+        if self.birth_year >= self.__current_year:
+            raise TypeError("Bigger than current year!")
 
     def is_adult(self):
-        if self.__birth_year - current_year == 18: #<
+        if self.__current_year - self.birth_year >= 18:
             print(True)
         else:
             print(False)
 
     def get_age(self):
-        print(current_year - self.__birth_year ) #Person's age
+        print(self.__current_year - self.birth_year)  # Person's age
 
     @classmethod
-    def get_total_persons(cls):#общее число созданных Человеков
+    def get_total_persons(cls):  # общее число созданных Человеков
         pass
 
     def talk(self):
         print("Hello World")
 
+
 class Teacher(Person):
 
-    def new_talk(self,ntalk):#Переопределние метода talk
+    def new_talk(self, ntalk):  # Переопределние метода talk
         self.talk = ntalk
         print("Greetings, I'm your teacher")
+
     def tech(self):
         print("Lesson started by Teacher")
 
-person1 = Person('Myname',2001)
-person2 = Person('Aiym',2000)
-person3 = Person('Alice',1990)
 
-teacher1 = Teacher('Airas',2001)
-teacher2 = Teacher('Daniiar',2000)
-teacher3 = Teacher('Abai',1998)
+person1 = Person('Myname', 2004)
+person2 = Person('Aiym', 1999)
+person3 = Person('Alice', 1990)
+
+print(person2.name)
+print(person2.is_adult())
+print(person2.get_age())
+
+
+
+teacher1 = Teacher('Airas', 2001)
+teacher2 = Teacher('Daniiar', 2000)
+teacher3 = Teacher('Abai', 1998)
